0bcdf6e501341091e51668a6906c1b254b3d3833
OrtalYona <ortalyona94@gmail.com>
2017-12-23 17:59:09 +0200

    2

--- Models/Order.cs
+++ Models/Order.cs
@@ -8,12 +8,12 @@ namespace CloudProject.Models
     {
 
     public int ID {get; set;}
-    public string FirstName {get; set;}
-    public string LastName {get; set;}
-    public int PhoneNumber {get; set;}
+    public String FirstName {get; set;}
+    public String LastName {get; set;}
+    public String PhoneNumber {get; set;}
     public int NumOfPeople {get; set;}
     public DateTime Date {get; set;}
-    public string time {get; set;}
+    public String time {get; set;}
 
     }
 
