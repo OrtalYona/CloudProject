6d0dcf65d56032187b9379cdfb7eb648a0cff0a2
OrtalYona <ortalyona94@gmail.com>
2017-12-25 22:28:04 +0200

    update

---
+++ Controllers/ManagmentController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CloudProject.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+
+namespace CloudProject.Controllers
+{
+    [Route("api/[controller]")]
+    
+    public class ManagmentController : Controller
+
+    {
+
+
+
+    }
+}
---
+++ Models/Manage.cs
@@ -0,0 +1,17 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace CloudProject.Models
+{
+
+    public class Manage
+    {
+
+    public int ID {get; set;}
+    public List<Account> Accounts{get; set;}
+
+
+
+    }
+}
