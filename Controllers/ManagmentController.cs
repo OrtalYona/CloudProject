10dbae3f5873618b67924ea5e1e2fefd798cacb7
OrtalYona <ortalyona94@gmail.com>
2017-12-25 22:36:06 +0200

    1

--- Controllers/ManagmentController.cs
+++ Controllers/ManagmentController.cs
@@ -16,7 +16,35 @@ namespace CloudProject.Controllers
 
     {
 
+        [HttpPost]
+        public async Task<dynamic> Post([FromBody]Account a)
+        {
 
+            var hc = Helpers.CouchDBConnect.GetClient("users");
+            var response = await hc.GetAsync("users/"+a._id);
+            if (response.IsSuccessStatusCode) {
+                Account account = (Account) JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync(),typeof(Account));
+                if (account.password.Equals(a.password)) {
+                    Token t = new Token();
+                    t._id = a._id+":token:"+Guid.NewGuid();
+                    t.create = DateTime.Now;
+                    t.ttl = 600;
+
+                    HttpContent htc = new StringContent(
+                        JsonConvert.SerializeObject(t),
+                        System.Text.Encoding.UTF8,
+                        "application/json"
+                    );
+
+                    await hc.PostAsync("users", htc);
+
+                    return t;
+                }
+            };
+
+            return -1;
+
+        }
 
     }
 }
